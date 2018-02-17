54be26e94a69f6c350187c103a34c745c6346c3c
Caius Durling <dev@caius.name>
2018-02-17 11:55:57 +0000

    First steps

---
+++ cli/mpuppet/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Welcome to mpuppet, your friendly masterless puppet runner")
+	fmt.Println("Goodbye, the pleasure has all been yours")
+}
